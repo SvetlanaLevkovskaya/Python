da3bc2939c01a6216f0046d568604e14ff7f0349
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-02-19 13:14:18 +0300

    Python
    Python

---
+++ Codewars/23_min_array.py
@@ -0,0 +1,12 @@
+def find_smallest_int(arr):
+    return sorted(arr)[0]
+
+
+print(find_smallest_int([78, 56, 232, 12, 11, -43]))
+
+
+def findSmallestInt(arr):
+    return min(arr)
+
+
+print(find_smallest_int([78, 56, 232, 12, 11, -43]))
