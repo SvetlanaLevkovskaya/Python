308b8577c99f5da2fa9526dab98e1302cf2fc09b
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-02-20 23:53:32 +0300

    Python
    Python

--- Codewars/45_alternating_case.py
+++ Codewars/45_alternating_case.py
@@ -4,4 +4,12 @@
 def to_alternating_case(string):
     return string.swapcase()
 
+
+print(to_alternating_case("ALTerNAtiNG CaSe"))
+
+
+def to_alternating_case(string):
+    return ''.join([c.upper() if c.islower() else c.lower() for c in string])
+
+
 print(to_alternating_case("ALTerNAtiNG CaSe"))
