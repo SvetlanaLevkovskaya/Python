21e647b71ee5851c64693ae8e8e677dd21848429
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-02-22 16:33:21 +0300

    Python
    Python

--- Codewars/71_planet_by_id_dict.py
+++ Codewars/71_planet_by_id_dict.py
@@ -16,3 +16,19 @@ def get_planet_name(id):
 
 
 print(get_planet_name(5))
+
+
+def get_planet_name(id):
+    return {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune",
+    }.get(id, None)
+
+
+print(get_planet_name(5))
---
+++ Codewars/72_sort_and_star.py
@@ -0,0 +1,12 @@
+"""You will be given a vector of strings. You must sort it alphabetically (case-sensitive,
+and based on the ASCII values of the chars) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array."""
+
+
+def two_sort(array):
+    return "***".join(sorted(array)[0])
+
+
+print(two_sort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+print(two_sort(["i", "want", "to", "travel", "the", "world", "writing", "code", "one", "day"]))
