0ffb13cad9babf9d33f5ad2b88b1e4d98cad918f
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-02-22 16:01:08 +0300

    Python
    Python

---
+++ Codewars/69_settence_smash.py
@@ -0,0 +1,8 @@
+"""['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'"""
+
+
+def smash(words):
+    return " ".join(words)
+
+
+print(smash(['hello', 'world', 'this', 'is', 'great']))
---
+++ Codewars/70_sum_array.py
@@ -0,0 +1,6 @@
+def sum_array(a):
+    return sum(a)
+
+
+print(sum_array([1.1, 2.2, 3.3]))
+print(sum_array(range(101)))
