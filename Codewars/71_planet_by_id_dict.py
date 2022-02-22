653440616240a4e071495ad93ed9341caed2a0da
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-02-22 16:18:40 +0300

    Python
    Python

---
+++ Codewars/71_planet_by_id_dict.py
@@ -0,0 +1,18 @@
+"""The function is not returning the correct values. Can you figure out why?"""
+
+
+def get_planet_name(id):
+    # This doesn't work; Fix it!
+    name = {
+         1: "Mercury",
+         2: "Venus",
+         3: "Earth",
+         4: "Mars",
+         5: "Jupiter",
+         6:"Saturn",
+         7: "Uranus",
+         8: "Neptune"}
+    return name[id]
+
+
+print(get_planet_name(5))
