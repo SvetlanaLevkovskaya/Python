e70cdc89f18c78f63f2883bfc9bbb1a44baba5b4
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-02-28 23:07:32 +0300

    Python
    Python

---
+++ Codewars/100_date_to_integer.py
@@ -0,0 +1,19 @@
+import datetime
+from datetime import date
+
+
+def period_is_late(last, today, cycle_length):
+    return (today - last).days > cycle_length
+
+
+first_date = date(2020, 10, 2)
+second_date = date(2020, 10, 30)
+delta = second_date - first_date
+print(delta)
+
+
+a = datetime.datetime.now()
+b = datetime.date.today()
+x = date(2022, 1, 21)
+print(type(x))
+print(datetime.timedelta())
