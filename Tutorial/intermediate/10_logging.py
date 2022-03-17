938d71a78a42dc1b8a68d9fe94fa78c11a9ae932
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-03-17 15:42:34 +0300

    Python
    Python

--- Tutorial/intermediate/10_logging.py
+++ Tutorial/intermediate/10_logging.py
@@ -1,6 +1,15 @@
 import logging
-logging.debug('This is debug message')
-logging.info('This is an info message')
-logging.warning('This is warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s  - %(levelname)s + %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S')
+
+# logging.debug('This is debug message')
+# logging.info('This is an info message')
+# logging.warning('This is warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical message')
+
+import helper
+
+
+
+
